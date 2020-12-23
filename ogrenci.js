2ef2a1aae8e582fa86b1aa00958bc5ac38628c07
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-12-23 12:00:58 +0300

    Update ogrenci.js

--- ogrenci.js
+++ ogrenci.js
@@ -21,7 +21,7 @@ if (window.location.toString().includes("liveMiddleware")) {
                     window.location = resp.liveLessonInfo.studyTime.registrantJoinUrl;
                 } else {
                     $.ajax({
-                        url: "https://uygulama.sebitvcloud.com/VCloudFrontEndService//livelesson/instudytime/join",
+                        url: "https://uygulama.sebitvcloud.com/VCloudFrontEndService/livelesson/inpage/instudytime/join",
                         method: "POST",
                         headers: {
                             "Content-Type": "application/x-www-form-urlencoded",
@@ -59,7 +59,7 @@ if (window.location.toString().includes("liveMiddleware")) {
     });
 } else {
     $.ajax({
-        url: "https://uygulama.sebitvcloud.com/VCloudFrontEndService//livelesson/instudytime/join",
+        url: "https://uygulama.sebitvcloud.com/VCloudFrontEndService//studytime/getstudentstudytime",
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -91,7 +91,7 @@ if (window.location.toString().includes("liveMiddleware")) {
             var selectedDers = prompt("Seçim yapınız (sadece rakam girin):\n\n" + dersText);
             var ders = dersler[parseInt(selectedDers) - 1];
             $.ajax({
-                url: "https://uygulama.sebitvcloud.com/VCloudFrontEndService//studytime/getstudentstudytime",
+                url: "https://uygulama.sebitvcloud.com/VCloudFrontEndService//livelesson/instudytime/join",
                 method: "POST",
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded",
