c9918237f476f1b351ef4dc2de12af5659733233
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-12-23 11:16:13 +0300

    Create sebit_nonce.php

---
+++ sebit_nonce.php
@@ -0,0 +1,9 @@
+// Forked from https://github.com/sh4dowb/
+<?php
+
+// Bu dosyayı kullanmanıza gerek yok. Geliştiriciler içindir.
+// EBA'ya nonce'dan Zoom tokeni için istek atarken CORS headeri olmadığı için JS'den yapamıyoruz.
+// Sunucuma eklediğim bu dosya ile header eklemiş oluyoruz ve JS'den kullanabiliyoruz.
+header("Access-Control-Allow-Origin: *");
+if(ctype_xdigit($_GET['nonce']))
+        echo file_get_contents("https://uygulama.sebitvcloud.com/VCloudFrontEndService/livelesson/nonce/".$_GET['nonce']);
