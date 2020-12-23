c572c9289b3bacbd520015c4ba6c1af8f77bf24e
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-12-23 11:33:42 +0300

    Update ogrenci.js

--- ogrenci.js
+++ ogrenci.js
@@ -91,9 +91,8 @@ if (window.location.toString().includes("liveMiddleware")) {
             var selectedDers = prompt("Seçim yapınız (sadece rakam girin):\n\n" + dersText);
             var ders = dersler[parseInt(selectedDers) - 1];
             $.ajax({
-                url: "http://uygulama.sebitvcloud.com/VCloudFrontEndService//livelesson/instudytime/join"
+                url: "http://uygulama.sebitvcloud.com/VCloudFrontEndService//livelesson/instudytime/join",
                 method: "POST",
-            }
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded",
                     "Accept": "json"
