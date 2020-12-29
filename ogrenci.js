ffb2f1b8d5580121269a6f19651c669a873d42ff
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-12-29 15:03:38 +0300

    Update ogrenci.js

--- ogrenci.js
+++ ogrenci.js
@@ -49,7 +49,7 @@ if (window.location.toString().includes("liveMiddleware")) {
                                         eventLabel: ""
                                     }); }catch(a){}
                                     console.log(resp3,resp2)
-                                    window.location = "https://us02web.zoom.us/j/"+resp.liveLessonInfo.studyTime.studyTimeId + "?tk=" + resp3.substring(1).split('|')[0];
+                                   window.location = resp2.meeting.url + "?tk=" + resp3.substring(1).split('|')[0];
                                 }
                             });
                         }
