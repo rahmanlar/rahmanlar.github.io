71ca694b3775a4bc4aba3d72aa770daf93d076db
Restyled.io <commits@restyled.io>
2020-12-29 12:36:51 +0000

    Restyled by prettier

--- ogrenci.js
+++ ogrenci.js
@@ -2,90 +2,104 @@
 
 if (window.location.toString().includes("liveMiddleware")) {
   $.ajax({
-    url : "https://sebitvcloud.com/getlivelessoninfo",
-    method : "GET",
-    headers : {"Accept" : "json"},
-    withCredentials : true,
-    crossDomain : true,
-    xhrFields : {withCredentials : true},
-    dataType : "json",
-    success : function(resp) {
-      if (resp.liveLessonInfo.studyTime == null)
-        alert('aktif canlÄ± ders yok');
+    url: "https://sebitvcloud.com/getlivelessoninfo",
+    method: "GET",
+    headers: { Accept: "json" },
+    withCredentials: true,
+    crossDomain: true,
+    xhrFields: { withCredentials: true },
+    dataType: "json",
+    success: function (resp) {
+      if (resp.liveLessonInfo.studyTime == null) alert("aktif canlÄ± ders yok");
       else {
         if (resp.liveLessonInfo.studyTime.registrantJoinUrl) {
           window.location = resp.liveLessonInfo.studyTime.registrantJoinUrl;
         } else {
           $.ajax({
-            url :
-                "https://uygulama.sebitvcloud.com/VCloudFrontEndService/livelesson/instudytime/join",
-            method : "POST",
-            headers : {
-              "Content-Type" : "application/x-www-form-urlencoded",
-              "Accept" : "json"
+            url:
+              "https://uygulama.sebitvcloud.com/VCloudFrontEndService/livelesson/instudytime/join",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              Accept: "json",
             },
-            data : "studytimeid=" + resp.liveLessonInfo.studyTime.studyTimeId +
-                       "&tokentype=nonce&platform=windows",
-            withCredentials : true,
-            crossDomain : true,
-            xhrFields : {withCredentials : true},
-            dataType : "json",
-            success : function(resp2) {
+            data:
+              "studytimeid=" +
+              resp.liveLessonInfo.studyTime.studyTimeId +
+              "&tokentype=nonce&platform=windows",
+            withCredentials: true,
+            crossDomain: true,
+            xhrFields: { withCredentials: true },
+            dataType: "json",
+            success: function (resp2) {
               if (resp2.success == false) {
-                alert("bir hata oluÅŸtu: " +
-                      resp2.operationMessage.replace('studytimenotstarted',
-                                                     'ders daha baÅŸlamadÄ±.'));
+                alert(
+                  "bir hata oluÅŸtu: " +
+                    resp2.operationMessage.replace(
+                      "studytimenotstarted",
+                      "ders daha baÅŸlamadÄ±."
+                    )
+                );
                 return;
               }
 
               $.ajax({
-                url : "https://cezeri.software/sebit.php?nonce=" +
-                          resp2.meeting.token,
-                success : function(resp3) {
+                url:
+                  "https://cezeri.software/sebit.php?nonce=" +
+                  resp2.meeting.token,
+                success: function (resp3) {
                   try {
-                    ga('send', 'event', {
-                      eventCategory : "liveLesson",
-                      eventAction : "join",
-                      eventLabel : ""
+                    ga("send", "event", {
+                      eventCategory: "liveLesson",
+                      eventAction: "join",
+                      eventLabel: "",
                     });
-                  } catch (a) {
-                  }
-                  console.log(resp3, resp2)
-                  window.location = "https://us02web.zoom.us/j/" +
-                                    "?tk=" + resp3.substring(1).split('|')[0];
-                }
+                  } catch (a) {}
+                  console.log(resp3, resp2);
+                  window.location =
+                    "https://us02web.zoom.us/j/" +
+                    "?tk=" +
+                    resp3.substring(1).split("|")[0];
+                },
               });
-            }
+            },
           });
         }
       }
-    }
+    },
   });
 } else {
   $.ajax({
-    url :
-        "https://uygulama.sebitvcloud.com/VCloudFrontEndService//studytime/getstudentstudytime",
-    method : "POST",
-    headers : {
-      "Content-Type" : "application/x-www-form-urlencoded",
-      "Accept" : "json"
+    url:
+      "https://uygulama.sebitvcloud.com/VCloudFrontEndService//studytime/getstudentstudytime",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "json",
     },
-    data : "status=1&type=2&pagesize=25&pagenumber=0",
-    withCredentials : true,
-    crossDomain : true,
-    xhrFields : {withCredentials : true},
-    dataType : "json",
-    success : function(resp) {
+    data: "status=1&type=2&pagesize=25&pagenumber=0",
+    withCredentials: true,
+    crossDomain: true,
+    xhrFields: { withCredentials: true },
+    dataType: "json",
+    success: function (resp) {
       var result = resp.studyTimeList;
       var dersler = [];
       var dersText = "";
       var id = 1;
       for (var i in result) {
-        if ((new Date).getTime() + 18000000 > result[i].startdate) {
+        if (new Date().getTime() + 18000000 > result[i].startdate) {
           dersler.push(result[i]);
-          dersText = dersText + (id.toString() + ") " + result[i].title + " (" +
-                                 result[i].ownerName + " " +
-                                 result[i].ownerSurname + ")\n");
+          dersText =
+            dersText +
+            (id.toString() +
+              ") " +
+              result[i].title +
+              " (" +
+              result[i].ownerName +
+              " " +
+              result[i].ownerSurname +
+              ")\n");
           id = id + 1;
         }
       }
@@ -93,49 +107,55 @@ if (window.location.toString().includes("liveMiddleware")) {
         alert("aktif ders yok");
         return;
       }
-      var selectedDers =
-          prompt("Seçim yapınız (sadece rakam girin):\n\n" + dersText);
+      var selectedDers = prompt(
+        "Seçim yapınız (sadece rakam girin):\n\n" + dersText
+      );
       var ders = dersler[parseInt(selectedDers) - 1];
       $.ajax({
-        url :
-            "https://uygulama.sebitvcloud.com/VCloudFrontEndService//livelesson/instudytime/join",
-        method : "POST",
-        headers : {
-          "Content-Type" : "application/x-www-form-urlencoded",
-          "Accept" : "json"
+        url:
+          "https://uygulama.sebitvcloud.com/VCloudFrontEndService//livelesson/instudytime/join",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Accept: "json",
         },
-        data : "studytimeid=" + ders.id + "&tokentype=nonce&platform=windows",
-        withCredentials : true,
-        crossDomain : true,
-        xhrFields : {withCredentials : true},
-        dataType : "json",
-        success : function(resp2) {
+        data: "studytimeid=" + ders.id + "&tokentype=nonce&platform=windows",
+        withCredentials: true,
+        crossDomain: true,
+        xhrFields: { withCredentials: true },
+        dataType: "json",
+        success: function (resp2) {
           if (resp2.success == false) {
-            alert("bir hata oluştu: " +
-                  resp2.operationMessage.replace('studytimenotstarted',
-                                                 'ders daha başlamadı.'));
+            alert(
+              "bir hata oluştu: " +
+                resp2.operationMessage.replace(
+                  "studytimenotstarted",
+                  "ders daha başlamadı."
+                )
+            );
             return;
           }
 
           $.ajax({
-            url : "https://cezeri.software/sebit.php?nonce=" +
-                      resp2.meeting.token,
-            success : function(resp3) {
+            url:
+              "https://cezeri.software/sebit.php?nonce=" + resp2.meeting.token,
+            success: function (resp3) {
               try {
-                ga('send', 'event', {
-                  eventCategory : "liveLesson",
-                  eventAction : "join",
-                  eventLabel : ""
+                ga("send", "event", {
+                  eventCategory: "liveLesson",
+                  eventAction: "join",
+                  eventLabel: "",
                 });
-              } catch (a) {
-              }
-              window.location = "https://us02web.zoom.us/j/" +
-                                resp.liveLessonInfo.studyTime.studyTimeId +
-                                "?tk=" + resp3.substring(1).split('|')[0];
-            }
+              } catch (a) {}
+              window.location =
+                "https://us02web.zoom.us/j/" +
+                resp.liveLessonInfo.studyTime.studyTimeId +
+                "?tk=" +
+                resp3.substring(1).split("|")[0];
+            },
           });
-        }
+        },
       });
-    }
+    },
   });
 }
